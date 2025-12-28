1d13257a9d335434ce905c16816727156db69d6a
vale-sketch <vale@electis.io>
2025-12-28 21:56:02 +0100

    Update generatev2.js

--- api/generate.js
+++ api/generate.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-  
+   
   if (req.method === 'OPTIONS') {
     console.log('✅ CORS preflight request handled');
     return res.status(200).end();
